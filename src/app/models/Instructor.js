25d502da52245f23444e02f9965e5965d57d11f0
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-12-25 13:16:31 +0000

    Desafio 5.2 in progress 0.2

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -1,3 +1,6 @@
+const { age, date } = require('../../lib/utils')
+const db = require('../../config/db')
+
 module.exports = {
     all(callback){
 
@@ -7,9 +10,33 @@ module.exports = {
             callback(results.rows)
         })
 
-    },s
+    },
 
-    /* create(){
+    create(data, callback){
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
+    const values = [
+        data.name,
+        data.avatar_url,
+        data.gender,
+        data.services,
+        date(data.birth).iso,
+        date(Date.now()).iso
+    ]
 
-    } */
+    db.query(query, values, function(err, results){
+        if(err) return res.send("Database Error!")
+
+        callback(results.rows[0])
+    })
+    }
 }
